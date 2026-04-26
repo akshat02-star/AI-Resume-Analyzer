4c282cf11f50a8c4cc7f47f4b95cd427cac368f7
Akshat Agrawal <akshat.agrawal@hyd-akshat.a>
2026-04-26 21:05:55 +0530

    First call to gemini with a very basic prompt

--- backend/app/services/analyzer.py
+++ backend/app/services/analyzer.py
@@ -1,2 +1,25 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
 async def analyze(content):
-    return f"{content}"
+    prompt = f"""
+    Analyze this resume and provide:
+    1. Resume score out of 100
+    2. Strengths
+    3. Weaknesses
+    4. Suggestions for improvement
+    Resume Content:
+    {content}
+    """
+
+    response = model.generate_content(prompt)
+
+    return response.text
