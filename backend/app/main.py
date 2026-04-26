1206b6725fc0064ac7b63b42c270250139462b86
Akshat Agrawal <akshat.agrawal@hyd-akshat.a>
2026-04-26 12:19:38 +0530

    Adding CORS header to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,17 +11,18 @@ from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 app = FastAPI(title="AI Resume Analyzer")
 
-# origins = [
-#     "http://localhost:5173",  # your frontend
-# ]
+origins = [
+    "http://localhost:5173", # for local development
+    "https://ai-resume-analyzer-sclr.onrender.com" # Render hosted backend server
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the routes from the api module
 app.include_router(api_router, prefix="/api/v1")
